52765417cd39fad794e63ce5264f877e7d464358
Sean Carver <seancarverphd@gmail.com>
2017-04-20 17:44:00 -0400

    Committing files from seancarver.org

---
+++ My_access_template.R
@@ -0,0 +1,7 @@
+consumer_key <- "paste-your-consumer-key-here"
+consumer_secret <- "paste-your-consumer-secret-here"
+access_token <- "paste-your-access-token-here"
+access_secret <- "paste-your-access-secret-here"
+
+library(twitteR)
+setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
---
+++ Wcloud.R
@@ -0,0 +1,38 @@
+##### EDIT SEARCH PARAMETERS BELOW
+search_term <- "compassion"
+language <- "en"
+sample_size <- 500
+##### EDIT SEARCH PARAMETERS ABOVE
+
+# NEXT 4 LINES LOAD LIBRARIES OF FUNCTIONS
+library(twitteR)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
+####### EDIT THE NEXT LINE TO SPECIFY SEARCH
+tweets_raw = searchTwitter(search_term, n=sample_size, lang=language)
+####### EDIT THE PREVIOUS LINE TO SPECIFY SEARCH
+
+# NEXT TWO LINES REMOVES META-DATA THEN CONVERTS DATA TO A NEEDED FORMAT
+tweets_text = sapply(tweets_raw, function(x) x$getText())
+tweets_corpus = Corpus(VectorSource(tweets_text))
+
+# NEXT LINE REMOVES SPECIAL CHARACTERS AVAILABLE IN OTHER LANGUAGES
+tweets_transformed1 <- tm_map(tweets_corpus, 
+                               content_transformer(function(x) iconv(x, to="UTF-8")),
+                               mc.cores=1)
+# NEXT LINE CONVERTS EVERYTHING TO LOWER CASE
+tweets_transformed2 <- tm_map(tweets_transformed1,
+                              content_transformer(tolower),
+                              mc.cores=1)
+# NEXT LINE REMOVES PUNCTUATION
+tweets_transformed3 <- tm_map(tweets_transformed2,
+                              removePunctuation,
+                              mc.cores=1)
+# NEXT LINE REMOVES "STOPWORDS" LIKE "THE", "AN", "IS", "BE", ETC.
+tweets_transformed4 <- tm_map(tweets_transformed3,
+                              function(x) removeWords(x,stopwords()),
+                              mc.cores=1)
+# NEXT LINE DRAWS WORD CLOUD
+wordcloud(tweets_transformed4)
